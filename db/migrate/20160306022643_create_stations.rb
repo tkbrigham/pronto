3b81af5e68a0dffff208bbf22c2ce31003ddf3d2
Thomas Brigham <thomas@thomasbrigham.me>
2016-03-05 18:26:59 -0800

    Add Station model

---
+++ db/migrate/20160306022643_create_stations.rb
@@ -0,0 +1,21 @@
+class CreateStations < ActiveRecord::Migration
+  def change
+    create_table :stations do |t|
+      t.integer :pronto_id
+      t.string :station_name
+      t.string :terminal_name
+      t.integer :status
+      t.boolean :blocked
+      t.boolean :suspended
+      t.boolean :out_of_service
+      t.datetime :latest_update
+      t.datetime :latest_communication
+      t.boolean :has_key_dispenser
+      t.boolean :has_keys_available
+      t.decimal :lat
+      t.decimal :lon
+
+      t.timestamps null: false
+    end
+  end
+end
