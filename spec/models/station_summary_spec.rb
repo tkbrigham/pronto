0bb28001221511623bd0aa0f59cb7c0e32a6577b
Thomas Brigham <thomas@thomasbrigham.me>
2016-03-11 13:38:18 -0800

    Clean up StationSummary spec

--- spec/models/station_summary_spec.rb
+++ spec/models/station_summary_spec.rb
@@ -4,13 +4,11 @@ RSpec.describe StationSummary, type: :model do
   let(:summary) { build(:station_summary) }
 
   it "should respond to associations" do
-    expect(summary).to respond_to(:station)
-    expect(summary).to respond_to(:stats)
+    expect(summary).to respond_to(:station, :stats, :last_stat)
   end
 
   describe "#last_stat" do
     it "should return StationStat" do
-      expect(summary).to respond_to(:last_stat)
       expect(summary.last_stat).to be_an_instance_of(StationStat)
     end
   end
