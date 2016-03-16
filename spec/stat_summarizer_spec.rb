4c1b60fa9ae13f364931f67d571cf1940c4e505d
Thomas Brigham <thomas@thomasbrigham.me>
2016-03-16 14:19:54 -0700

    Further implementation

--- spec/stat_summarizer_spec.rb
+++ spec/stat_summarizer_spec.rb
@@ -3,10 +3,46 @@ require 'rails_helper'
 RSpec.describe StatSummarizer do
   let(:stat_not_summed) { create(:station_stat, timestamp: 800) }
   let(:stat_summed) { create(:station_stat) }
-  let!(:summary) { create(:station_summary, :avg_bike_of_ten, :two_samples,
+  let!(:summary) { create(:station_summary, :ten_bikes, :two_samples,
                           station: stat_summed.station,
-                          timestamp: stat_summed.timestamp)
-  }
+                          timestamp: stat_summed.timestamp) }
+  let(:summarizer) { StatSummarizer.new(stat_summed) }
+
+  #describe "#stat" do
+  #  it "will respond" do
+  #    expect(summarizer).to respond_to(:stat)
+  #    expect(summarizer.stat).to be_a(StationStat)
+  #  end
+
+  #  context 'with existing summary' do
+  #    it 'returns stat with same timestamp as summary' do
+  #      expect(summarizer.stat.timestamp).to eq(summarizer.summary.timestamp)
+  #    end
+
+  #    it 'returns stat with same station as summary' do
+  #      expect(summarizer.stat.station).to eq(summarizer.summary.station)
+  #    end
+  #  end
+  #end
+
+  #describe "#summary" do
+  #  it "will respond" do
+  #    expect(summarizer).to respond_to(:summary)
+  #  end
+
+  #  context "with existing summary" do
+  #    it "returns a StationSummary" do
+  #      expect(summarizer.summary).to be_a(StationSummary)
+  #    end
+  #  end
+
+  #  context "with no summary" do
+  #    let(:summarizer) { StatSummarizer.new(stat_not_summed) }
+  #    it "returns nil" do
+  #      expect(summarizer.summary).to be_nil
+  #    end
+  #  end
+  #end
 
   describe "#find_or_create_summary" do
     it "creates StationSummary if it doesn't exist" do
@@ -21,16 +57,25 @@ RSpec.describe StatSummarizer do
     end
   end
 
+  describe "#increment_field" do
+    it "updates the StationSummary field" do
+      ct = summarizer.stat.bikes_a
+      expect{summarizer.increment_field(:tot_bike_a)}.
+        to change{summarizer.summary.tot_bike_a}.by(ct)
+    end
+  end
+
   describe "#summarize" do
-    it "updates StationSummary average" do
-      # Set the StationSummary
-      # Set a new StationStat
-      # Set the StatSummarizer with that Stat
-      summarizer = StatSummarizer.new(stat_summed)
-      # Calculate The new station summary
-      summarizer.summarize
-      # Verify
-      expect(summary.avg_dock_a.to_s).to eq('3.67')
+    it "updates StationSummary totals" do
+      map = StatSummarizer::INC_FIELDS
+      map.each_key do |k|
+        ct = summarizer.stat.send(map[k])
+        expect{summarizer.summarize}.to change{summarizer.summary.send(k)}.by(ct)
+      end
+    end
+
+    it "updates samples" do
+      expect{summarizer.summarize}.to change{summarizer.summary.samples}.by(1)
     end
   end
 
